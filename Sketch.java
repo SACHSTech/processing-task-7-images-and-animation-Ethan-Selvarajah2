8f2e149f842aae343661eed8199812cc97306d0b
Ethan-Selvarajah2 <ethan.selvarajah25@ycdsbk12.ca>
2023-12-05 18:56:50 -0500

    5.9 Processing Task 7 - Swaggy Duck Code

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,60 @@
 import processing.core.PApplet;
+import processing.core.PImage;
+import java.lang.Math;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  PImage imgDuckWalk;
+  PImage imgBackground;
+  float fltDuckX = 150;
+  float fltDuckY = 150;
+  float fltRectX = 100;
+  float fltRectY = 100;
+  int xDirection = 1;
+  int yDirection = 1;
+
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    // Setup images
+    imgDuckWalk = loadImage("swaggy_duck_transparent_1.png");
+    imgBackground = loadImage("sky-with-clouds-and-sun-shining-through-8-bit.jpg");
+
+    // Resize images
+    imgDuckWalk.resize(imgDuckWalk.width / 5, imgDuckWalk.height / 5);
+    imgBackground.resize(imgBackground.width / 2, imgBackground.height / 2);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Background drawn
+    image(imgBackground, -100, -50);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Duck drawn
+    image(imgDuckWalk, fltDuckX, fltDuckY);
+
+    // Duck Movement
+    fltDuckX += (float)(Math.pow(2 * xDirection, 2) + (3 * xDirection) + yDirection);
+    fltDuckY = (float)(height - 0.02 * Math.pow(fltDuckX - width / 4, 2));
+
+
+    // Shape drawn and moved
+    rect(fltRectX, fltRectY, 30, 30);
+    fltRectX -= 2;
+    fltRectY -= 2;
+    
+    // Edge detection for duck
+    if (fltDuckX >= width + imgDuckWalk.width / 2) {
+      fltDuckX = -imgDuckWalk.width / 2;
+    } else if (fltDuckX <= -imgDuckWalk.width / 2) {
+      fltDuckX = width + imgDuckWalk.width / 2;
+    }
+
+    // Edge detection for rectangle
+    if (fltRectX <= -30) {
+      fltRectX = width;
+    } else if (fltRectY <= -30) {
+      fltRectY = height;
+    }
   }
-  
-  // define other methods down here.
 }
